50422d0df9e9524fd4854667720e0a0bcb24c3a2
aleks <aleksbuss@gmail.com>
2024-02-12 18:34:26 +0100

    12.02.24

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -11,15 +11,54 @@ export default function Catalog() {
 
   return (
     <Wrapper>
-      <div className={s.catalog_wrapper}>
+    <div className={s.catalog_wrapper}>
+      <div className={s.buttons_row}>
         <div className={s.title_button}>
           <p>Catalog</p>
           <Link to="/categories">
             <button className={s.all_categories}>All categories</button>
           </Link>
         </div>
-        <CategoryContainer categories={catalog} />
+        <div className={`${s.wild_sale_button} ${s.wild_sale}`}>
+          <Link to="/sales">
+            <button className={s.all_sales}>Wild Sale</button>
+          </Link>
+        </div>
       </div>
-    </Wrapper>
+      <CategoryContainer categories={catalog} />
+    </div>
+  </Wrapper>
   );
 }
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import s from "./style.module.css";
+
+// import { Link } from "react-router-dom";
+// import CategoryContainer from "../CategoryContainer";
+// import Wrapper from "../UI/Wrapper";
+
+// export default function Catalog() {
+//   const categories = useSelector((state) => state.categories);
+//   const catalog = categories.slice(0, 4);
+
+//   return (
+//     <Wrapper>
+//       <div className={s.catalog_wrapper}>
+//         <div className={s.title_button}>
+//           <p>Catalog</p>
+//           <Link to="/categories">
+//             <button className={s.all_categories}>All categories</button>
+//           </Link>
+//         </div>
+// i        <div className={`${s.title_button} ${s.wild_sale}`}> 
+//           <Link to="/sales">
+//             <button className={s.all_sales}>WILD SALE</button>
+//           </Link>
+//         </div>
+
+//         <CategoryContainer categories={catalog} />
+//       </div>
+//     </Wrapper>
+//   );
+// }
--- src/components/CategoryItem/index.jsx
+++ src/components/CategoryItem/index.jsx
@@ -1,20 +1,56 @@
-import React from 'react'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import s from './style.module.css'
+// import Wrapper from '../UI/Wrapper'
+
+// export default function CategoryItem({ id, image, title }) {
+
+//   const img = `http://localhost:3333/${image}`
+
+//   return (
+//     <Wrapper>
+//     <Link to={`/categories/${id}`}>
+//       <div className={s.categoryItem}>
+//         {/* <p>{title }</p> */}
+//             <img src={img} alt="" />
+//         </div>
+//     </Link>
+//     </Wrapper>
+//   )
+// }
+
+
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import s from './style.module.css'
 import Wrapper from '../UI/Wrapper'
 
 export default function CategoryItem({ id, image, title }) {
-
+  const [isPressed, setIsPressed] = useState(false);
   const img = `http://localhost:3333/${image}`
+
+  const handleMouseDown = () => {
+    setIsPressed(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsPressed(false);
+  };
 
   return (
     <Wrapper>
-    <Link to={`/categories/${id}`}>
-      <div className={s.categoryItem}>
-        {/* <p>{title }</p> */}
-            <img src={img} alt="" />
+      <Link to={`/categories/${id}`}>
+        <div className={s.categoryItem}>
+          <img 
+            src={img} 
+            alt="" 
+            onMouseDown={handleMouseDown} 
+            onMouseUp={handleMouseUp} 
+            onMouseLeave={handleMouseUp} // Ensure the state is reset even if the mouse leaves the image while pressed
+            className={isPressed ? s.pressed : ''}
+          />
         </div>
-    </Link>
+      </Link>
     </Wrapper>
   )
 }
